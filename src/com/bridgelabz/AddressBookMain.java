a859b465eae9cf815de52ebefd1061f7ddf04c98
anjirajardula <anjirajardula@gmail.com>
2021-09-06 17:44:14 +0530

    Added:edit contact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -5,11 +5,21 @@ Defining newContact Object
 And Printing The Contact Details
  */
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.print("Welcome to Address Book Program \n");
+        Scanner sc = new Scanner(System.in);
         NewAddressBook person = new NewAddressBook();
         person.addContact();
+        System.out.println("Enter Y To Edit The Contact");
+        String op = sc.nextLine();
+        //If Condition To Edit The Contact Details Of Person
+        if (op.equals("y") || op.equals("Y")) {
+            person.editContact(person); //Calling The Edit Contact Method
+            System.out.println("You have Entered following data");
+            System.out.println("The Contact Details After Editing : " + person);
 
 
     }
@@ -17,4 +27,4 @@ public class AddressBookMain {
 
 
 
-}
+}}
--- src/com/bridgelabz/NewAddressBook.java
+++ src/com/bridgelabz/NewAddressBook.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class NewAddressBook {
     Scanner sc = new Scanner(System.in);
+    private String firstName;
       /*
     Declaring The Add Contact Method
     And Entering The Contact Details By Using Scanner Class
@@ -38,5 +39,27 @@ public class NewAddressBook {
         person.setEmail(email);
         System.out.println("The Contact Details of "+firstName+ "\n"+ person);
     }
+    /*
+    Declaring The Edit Contact Method
+    TO Edit The Details Of Contact
+    The Details Of Contact Edit By Using FirstName
+    If First Name Is Match The Contact Will Edit
+    */
+    public void editContact(NewAddressBook person){
+        //Scanner editName = new Scanner(System.in);
+        System.out.println("Enter the firstName of person");
+        String editName = sc.nextLine();
+        if (editName.equalsIgnoreCase(person.getfirstName()));
+        addContact();
+
+        System.out.println("The Entered First Name Is Not Match");
+        editContact(person);
+    }
+
+    private String getfirstName() {
+        return firstName;
+    }
+
 
 }
+
