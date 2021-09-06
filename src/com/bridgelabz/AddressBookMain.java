a21ad90770b1435b5b464deff70ed4004f729b48
anjirajardula <anjirajardula@gmail.com>
2021-09-06 16:41:50 +0530

    Added:ability to add new contact

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -8,16 +8,9 @@ And Printing The Contact Details
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.print("Welcome to Address Book Program \n");
+        NewAddressBook person = new NewAddressBook();
+        person.addContact();
 
-        ContactDetails newContact = new ContactDetails();
-        newContact.setFirstName("anjiraj\n");
-        newContact.setLastName("ardula \n");
-        newContact.setAddressCity("nalgonda \n");
-        newContact.setState("Telangana \n");
-        newContact.setZip(508207);
-        newContact.setPhoneNumber(833877108);
-        newContact.setEmail("anjirajardula@gmail.com .\n");
-        System.out.println("The Contact is : \n" +newContact);
 
     }
 
